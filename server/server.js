ca3f8d960c55073f4a12c20d8abe52fea017d3b7
Abdibasid-Moh-aden0005 <abdibasid864@gmail.com>
2026-07-02 14:11:18 +0300

    Fix path references in package.json and enhance dotenv configuration in server.js

--- server/server.js
+++ server/server.js
@@ -14,11 +14,11 @@ import adminRoutes from "./routes/adminRoutes.js";
 // Database Connection
 import connectDB from "./config/database.js";
 
-// Load environment variables
-dotenv.config();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Load environment variables
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 
